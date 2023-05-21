389e8c8beee3ef2590874fb1a0a335a0967d0519
camilogs <camilo.gonzalez.col@gmail.com>
2023-05-21 12:17:13 -0500

    Add authenticate protocol

---
+++ Client.cs
@@ -0,0 +1,62 @@
+using System.Net.Sockets;
+using System.Text;
+
+namespace dotnetESL.Client;
+
+public class Client
+{
+    private Socket _socket;
+    private string HOST;
+    private int PORT;
+
+    public Client(string host, int port)
+    {
+        _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        HOST = host;
+        PORT = port;
+    }
+
+    public void Connect()
+    {
+        _socket.Connect(HOST, PORT);
+    }
+
+    public string Authenticate(string password)
+    {
+        string authRequest = RecolectData(); 
+
+        SendData($"auth {password}");
+
+        string authResponse = RecolectData(); 
+
+        return authResponse;
+    }
+
+    private void SendData(string data)
+    {
+        byte[] msg = Encoding.ASCII.GetBytes($"{data}\n\n");
+        _socket.Send(msg);
+    }
+
+    private string RecolectData()
+    {
+        string dataBuffer = "";
+
+        while (!dataBuffer.EndsWith("\n\n"))
+        {
+            dataBuffer += ReceiveData();
+        }
+
+        return dataBuffer;
+    }
+
+    private string ReceiveData()
+    {
+        byte[] buffer = new byte[10000];
+        int bytesReceived = _socket.Receive(buffer);
+        
+        string data = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+
+        return data;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using dotnetESL.Client;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var client = new Client("mob.bit4bit.in", 60555);
+
+        client.Connect();
+
+        Console.WriteLine(client.Authenticate("<password>"));
+    }
+}
