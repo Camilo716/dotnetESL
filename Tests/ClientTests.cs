cf1ffa2f19064465f54f4adb572f422eaa553eeb
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-08 17:26:50 -0500

    Configure tests' setup and teardown

--- Tests/ClientTests.cs
+++ Tests/ClientTests.cs
@@ -4,24 +4,31 @@ public class ClientTests
 {
     private const string HOST = "172.16.238.10";
     private const int PORT = 8020;
-    private Client _client;
+    private Client _client = null!;
 
     [SetUp]
     public void SetUp()
     {
         _client = new Client(HOST, PORT);
+        _client.Connect();
     }
 
     [Test]
     public void AuthenticationTest()
     {
-        _client.Connect();
-        
-        string authResponse = _client.Authenticate(password:"ClueCon");
+        string password = "ClueCon";
+
+        string authResponse = _client.Authenticate(password);
 
         string succesfullAuthResponse =
         "Content-Type: command/reply\n"+
         "Reply-Text: +OK accepted\n\n";
         Assert.That(authResponse, Is.EqualTo(succesfullAuthResponse));
     }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _client.CloseConnection();
+    } 
 }
