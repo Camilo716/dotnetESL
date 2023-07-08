fc4e24a3527e057b31333bef16f8be62a012a700
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-08 16:35:39 -0500

    Refactor: Change to sln architecture program

--- dotnetESL/Program.cs
+++ dotnetESL/Program.cs
@@ -4,17 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        // var client = new Client("172.16.238.10", 8020);
+        var client = new Client("172.16.238.10", 8020);
 
-        // client.Connect();
+        client.Connect();
 
-        // Console.WriteLine(client.Authenticate("ClueCon"));
+        Console.WriteLine(client.Authenticate("ClueCon"));
 
-        // Console.WriteLine(client.Uptime());
+        Console.WriteLine(client.Uptime());
 
         string packageDataUnit = "Content-Type: api/response\nContent-Length: 5123\n\n";
 
         var lines = packageDataUnit.Split();
+        
+
         
         System.Console.WriteLine();
     }
