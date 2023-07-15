3d4c9ffde2f506a5c77ddad29be3ae261d83cd0d
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-14 20:05:45 -0500

    Implement GetContentLenght test

--- dotnetESL/Util/DpuParser.cs
+++ dotnetESL/Util/DpuParser.cs
@@ -4,7 +4,22 @@ public static class DpuParser
 {
     public static int GetContentLenght(string dpu)
     {
-        return default;
+        string[] lines = dpu.Split('\n');
+        foreach (string line in lines)
+        {
+            if (line.StartsWith("Content-Length:"))
+            {
+                string lengthString = line.Substring("Content-Length:".Length).Trim();
+                int length;
+                if (int.TryParse(lengthString, out length))
+                {
+                    return length;
+                }
+                break;
+            }
+        };
+
+        return -1;
     }
 }
 
