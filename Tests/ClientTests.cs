bb6c929181e1326e07e7fe84c978391c2546fd3e
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-14 21:21:58 -0500

    Refactor: Rename assertion

--- Tests/ClientTests.cs
+++ Tests/ClientTests.cs
@@ -33,8 +33,8 @@ public class ClientTests
 
         int uptimeResponse = _client.Uptime();
 
-        bool timeIsRunning = uptimeResponse > 0;
-        Assert.True(timeIsRunning);
+        bool timeIsRunningAndResponseIsANumber = uptimeResponse > 0;
+        Assert.True(timeIsRunningAndResponseIsANumber);
     }
 
     [TearDown]
